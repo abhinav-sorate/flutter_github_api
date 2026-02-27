131766a0d01c7602da00d5c38f0554baa5fe175b
Abhinav Sorate <abhinavsorate01@gmail.com>
2026-02-27 17:53:58 +0530

    feat: add rxdart & implement debouncing

--- lib/core/bloc/user_bloc.dart
+++ lib/core/bloc/user_bloc.dart
@@ -1,15 +1,23 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:flutter_github_api/core/constants/status.enum.dart';
 import 'package:flutter_github_api/core/repository/user_repository.dart';
 
 part 'user_event.dart';
 part 'user_state.dart';
 
+EventTransformer<T> debounce<T>(Duration duration) {
+  return (events, mapper) => events.debounceTime(duration).switchMap(mapper);
+}
+
 class UserBloc extends Bloc<UserEvent, UserState> {
   final UserRepo _userRepo;
   UserBloc(this._userRepo) : super(UserState()) {
-    on<SearchUsers>(_searchUsers);
+    on<SearchUsers>(
+      _searchUsers,
+      transformer: debounce(const Duration(milliseconds: 500)),
+    );
   }
 
   Future<void> _searchUsers(SearchUsers event, Emitter<UserState> emit) async {
