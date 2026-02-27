574f43c252ba50071c66f9930598d2ebcbc4f40e
Abhinav Sorate <abhinavsorate01@gmail.com>
2026-02-28 01:01:28 +0530

    feat: add user header content in user details page

--- lib/features/users/presentation/screens/user_details_screen.dart
+++ lib/features/users/presentation/screens/user_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_github_api/core/constants/status.enum.dart';
 import 'package:flutter_github_api/features/users/presentation/bloc/user_bloc.dart';
 
 class UserDetailsScreen extends StatefulWidget {
@@ -20,8 +21,84 @@ class _UserDetailsScreenState extends State<UserDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.username)),
-      body: Column(children: []),
+      appBar: AppBar(title: const Text("User Details")),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(children: [_buildUserHeader()]),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _buildUserHeader() {
+  return BlocBuilder<UserBloc, UserState>(
+    builder: (context, state) {
+      final user = state.userDetails!;
+
+      if (state.getUserDetailsStatus.isLoading) {
+        return const Center(child: CircularProgressIndicator());
+      }
+
+      if (state.getUserDetailsStatus.isSuccess) {
+        return Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(user.avatarUrl),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                user.username,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 4),
+              Text(user.fullname, style: TextStyle(color: Colors.grey)),
+              const SizedBox(height: 16),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _FollowInfo(title: "Followers", count: user.followers),
+                  SizedBox(width: 24),
+                  _FollowInfo(title: "Following", count: user.following),
+                ],
+              ),
+            ],
+          ),
+        );
+      }
+
+      if (state.getUserDetailsStatus.isError) {
+        return Center(child: Text('Something went wrong'));
+      }
+
+      return const Center(child: Text('User Details'));
+    },
+  );
+}
+
+class _FollowInfo extends StatelessWidget {
+  final String title;
+  final int count;
+
+  const _FollowInfo({required this.title, required this.count});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          count.toString(),
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        Text(title, style: const TextStyle(color: Colors.grey)),
+      ],
     );
   }
 }
